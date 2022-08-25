9f234e95761c7c083020a57b2be603660229c3ce
Thais Possamai <100890747+thaispossamaisoares@users.noreply.github.com>
2022-08-25 20:36:35 -0300

    Add files via upload
    tabuada

---
+++ Tabuada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Aula
+{
+	class Tabuada
+	{
+		public static void Main(string[] args)
+		{
+
+		   Console.WriteLine(" Digite um número ");
+            int tabuad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine(tabuad + " * " + x + " = " + tabuad * x);
+
+                //Console.ReadLine();
+            }
+        }
+	}
+}
