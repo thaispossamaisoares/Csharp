f7b2c757b73628054ad9c9294528014e42da7fef
Thais Possamai <100890747+thaispossamaisoares@users.noreply.github.com>
2022-08-25 19:29:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace projeto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe a largura!");
+                double largura = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Informe a altura!");
+                double altura = Convert.ToDouble(Console.ReadLine());
+
+                double area = CalculaArea(largura,altura);
+                Console.WriteLine("A area equivale a:");
+                Console.WriteLine(area);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+                throw;
+            }
+
+        }
+
+        static double CalculaArea(double largura, double altura)
+        {
+            return largura * altura;
+        }
+
+    }
+}
+
